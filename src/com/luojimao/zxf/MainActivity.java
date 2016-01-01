f04e376cf8e3d4cf00ad0c02b80b4b3d679e9288
kevin_zheng <admin@360flyx.com>
2016-01-02 05:52:45 +0800

    修改了首页的说明文件

--- src/com/luojimao/zxf/MainActivity.java
+++ src/com/luojimao/zxf/MainActivity.java
@@ -2,7 +2,12 @@ package com.luojimao.zxf;
 
 import android.app.Activity;
 import android.os.Bundle;
-
+/**
+ * 
+ * @author zhengxiaofan
+ * 这是一个聚合多种功能的安卓app Demo,服务器端的业务数据和文件处理接口采用golang生成
+ *
+ */
 public class MainActivity extends Activity {
 
 	@Override
