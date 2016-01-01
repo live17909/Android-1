53994283b7e14c23df86eeca31f4131fd71e463e
kevin_zheng <admin@360flyx.com>
2016-01-02 06:32:12 +0800

    增加了主UI框架的fragmet+viewpager的布局文件

--- src/com/luojimao/zxf/MainActivity.java
+++ src/com/luojimao/zxf/MainActivity.java
@@ -6,7 +6,7 @@ import android.os.Bundle;
  * 
  * @author zhengxiaofan
  * 这是一个聚合多种功能的安卓app Demo,服务器端的业务数据和文件处理接口采用golang生成
- *
+ * 类说明:主题UI采用viewpager+fragment,可以滑动和点击切换tab
  */
 public class MainActivity extends Activity {
 
